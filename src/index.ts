d5a5c4e32aa3df6a75870d804452d8f1db8f4c26
Zakhar Krauchuk <Zakhar.Krauchuk@intexsoft.by>
2022-10-03 14:54:42 +0300

    add missed interface

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export {
     UserLoyalty,
     LoyaltyHistoryStatuses,
     LoyaltyHistoryDestinations,
+    LoyaltyHistoryDestination,
     LoyaltyHistoryStatus,
     LoyaltyHistoryItem,
     EditLoyaltyType
